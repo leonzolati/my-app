eec2d845a917b61cc931bd818b23ac45050ad6e9
leonzolati <leonzolati@gmail.com>
2024-06-02 15:01:14 +1000

    text styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
 import React from 'react';
 import './App.scss';
+import './Style.scss';
 
 function App() {
   return (
     <div className="App">
       <div className='Home'>
+        <h1>
+          Header 1 <br/>
+          Header 1
+        </h1>
+
+        <h2>
+          Header 2
+        </h2>
+
+        <h3>
+          Header 3
+        </h3>
+
+        <p>
+          paragraph
+        </p>
+
+        <a href=''>
+          link
+        </a>
       </div>
     </div>
   );
