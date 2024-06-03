e727b1e20aa8ae4b8c65db69c100394f98ff14fd
leonzolati <leonzolati@gmail.com>
2024-06-03 10:34:43 +1000

    fix scale and accessability

--- src/contact/contactButton.tsx
+++ src/contact/contactButton.tsx
@@ -3,7 +3,7 @@ import "./contactButton.scss"
 
 function ContactButton(props: { svgPath: string, svgAlt: string, title: string, link: string}) {
     return (
-        <a href={props.link} target="_blank" rel="noopener noreferrer">
+        <a href={props.link} target="_blank" rel="noopener noreferrer" >
             <div className='ContactButton'>
                 <img src={props.svgPath} alt={props.svgAlt} />
                 <h3>{props.title}</h3>
