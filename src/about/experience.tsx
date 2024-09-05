2852bcc0c4517cd745aa3530c1469998eabab0c9
leonzolati <leonzolati@gmail.com>
2024-09-05 10:42:25 +0700

    changes made to be more mobile friendly

--- src/about/experience.tsx
+++ src/about/experience.tsx
@@ -1,18 +1,30 @@
-import "./experience.scss"
+import "./experience.scss";
 
-function Experience(props: { svgPath: string, svgAlt: string, title: string, subTitle: string, description: string }) {
-    return (
-        <div className='Experience'>
-            <img src={props.svgPath} alt={props.svgAlt} />
-            <div id='text'>
-                <div>
-                    <h2>{props.title}</h2>
-                    <h3>{props.subTitle}</h3>
-                </div>
-                <p>{props.description}</p>
-            </div>
-        </div>
-    );
+function Experience(props: {
+	svgPath: string;
+	svgAlt: string;
+	title: string;
+	subTitle: string;
+	description: string;
+}) {
+	return (
+		<div className="Experience">
+
+			<div id="experienceImage">
+				<img src={props.svgPath} alt={props.svgAlt} />
+			</div>
+
+			<div id="experienceTitle">
+				<h2>{props.title}</h2>
+				<h3>{props.subTitle}</h3>
+			</div>
+
+			<div id="experienceDescription">
+				<p>{props.description}</p>
+			</div>
+
+		</div>
+	);
 }
 
 export default Experience;
